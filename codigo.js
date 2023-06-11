8e4d1d5bebfc14c12e38ff462e7a8ae88c4c1af2
Jardel Bastos <jardeljbastos@gmail.com>
2023-06-11 13:28:16 -0300

    Follow with knowledge JavaScript.

--- codigo.js
+++ codigo.js
@@ -1,2 +1,12 @@
-console.log('Olá Mundo!');
-console.log('Aqui segue uma nova mudança no meu código JavaScript utilizando Visual Studio Code e NodeJS!!!');
+//const mensagemTela = 'Olá Mundo! Meu nome é Jardel Bastos. Tenho 35 anos e serei um Perito Digital!';
+let mensagemTela = 'Olá Mundo! Meu nome é Jardel Bastos. Tenho 35 anos e serei um Perito Digital!';
+
+console.log(mensagemTela);
+console.log(mensagemTela);
+console.log(mensagemTela);
+console.log(mensagemTela);
+console.log(mensagemTela);
+
+mensagemTela = "Agora o que eu quero ser é um imigrante!";
+
+console.log(mensagemTela);
