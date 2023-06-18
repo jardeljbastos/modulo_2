88643501c7b98d245f13a92326ddcca6e3351611
Jardel Bastos <jardeljbastos@gmail.com>
2023-06-18 13:03:14 -0300

    New class concluded.

---
+++ comparadores.js
@@ -0,0 +1,19 @@
+let x = 2;
+let y = "string de teste";
+
+console.log(`01:${x > 1}`);
+console.log(`02:${x > 10}`);
+
+console.log(`03:${y === "string de teste"}`);
+console.log(`03a:${y === "novo texto que eu inventei"}`);
+console.log(`04:${y !== "novo texto que eu inventei"}`);
+
+console.log(`05:${x >= 2}`);
+console.log(`06:${x >= 1}`);
+console.log(`07:${x >= 3}`);
+console.log(`08:${x <= 3}`);
+console.log(`09:${x < 10}`);
+console.log(`10:${10 === 10}`);
+console.log(`11:${10 === '10'}`);
+console.log(`10:${10 == 10}`);
+console.log(`11:${10 == '10'}`);
