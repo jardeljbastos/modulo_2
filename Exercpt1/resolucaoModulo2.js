da2beb53e6a5abccb04cc6b96cb2698c161e90fc
Jardel Bastos <jardeljbastos@gmail.com>
2023-06-18 12:10:34 -0300

    News resolutions exercices JavaScript. Let's go.

---
+++ Exercpt1/resolucaoModulo2.js
@@ -0,0 +1,29 @@
+console.log("Resultado do exercicio 1");
+
+const inicial = 2148;
+let segunda;
+
+console.log(`O valor da primeira variável é ${inicial}`);
+
+segunda = 32;
+let soma = inicial + segunda;
+
+console.log(`Minha segunda variável vale ${segunda}`);
+console.log(`O valor da soma das minhas variáveis é ${soma}`);
+
+let eleva = segunda**2;
+console.log(eleva);
+console.log(eleva/3);
+
+console.log("Resultado do exercicio 2");
+
+let boole1 = false;
+let boole2 = true;
+
+console.log(boole1 && boole2);
+console.log(boole1 || boole2);
+
+let boole3 = true;
+
+console.log(boole1 && boole2 && boole3);
+console.log(boole1 || boole2 || boole3);
