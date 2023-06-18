1863f027a140736c8e4ccec326209a6d4966e638
Jardel Bastos <jardeljbastos@gmail.com>
2023-06-18 11:26:51 -0300

    News classes JavaScript

---
+++ booleans.js
@@ -0,0 +1,12 @@
+const valor1 = true;
+const valor2 = false;
+
+const valor1Evalor2 = valor1 && valor2;
+const valor1OUvalor2 = valor1 || valor2;
+const inversovalor1 = !valor1;
+const inversovalor2 = !valor2;
+
+console.log(valor1Evalor2);
+console.log(valor1OUvalor2);
+console.log(inversovalor1);
+console.log(inversovalor2)
---
+++ operadores.js
@@ -0,0 +1,25 @@
+
+2 + 3
+2 - 3
+2 * 3
+2 / 3
+2 ** 3; //Exponenciação
+2 % 3; //Resto da divisão (Módulos)
+
+//++ Incremento
+let x = 27;
+
+x++;
+x--; //-- Decremento
+
+const texto1 = "Oi, sou Gentil! ";
+const texto2 = "Tenho 40 anos.";
+
+const textoFinal = texto1 + texto2;
+
+const variavelNumerica1 = 27;
+const variavelNumerica2 = 35;
+
+const resulMult = variavelNumerica1 * variavelNumerica2;
+console.log(textoFinal);
+console.log(resulMult);
